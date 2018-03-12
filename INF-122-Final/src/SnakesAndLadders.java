352191a1145fdd46e11d5ddb80cea3416d325b54
Christine Yuen <christineyuenn@gmail.com>
2018-03-12 00:24:46 -0700

    Corrected push: Updated with snakes and ladder logic

--- INF-122-Final/src/SnakesAndLadders.java
+++ INF-122-Final/src/SnakesAndLadders.java
@@ -1,144 +1,145 @@
+package test;
 import java.util.*;
-import javafx.util.Pair;
 
-class SnakesAndLadders extends GridGame{
-  private static final int BOARD_SIZE = 10;
-	private int turn = 1;
+class SnakesAndLadders {
+	private static final int BOARD_SIZE = 10;
+	private int turn = 1; // Game starts with Player 1's turn
 	private Dice dice;
-	private int winner;
-	private HashMap<Integer, int[]> positionOfPlayers = new HashMap<Integer, int[]>();
-	private HashMap<int[], int[]> specialSpaces = new HashMap<int[], int[]>();
-	
-			
+	private HashMap<Integer, ArrayList<Integer>> positionOfPlayers = new HashMap<Integer, ArrayList<Integer>>();
+	static HashMap<ArrayList<Integer>, ArrayList<Integer>> specialSpaces = new HashMap<ArrayList<Integer>, ArrayList<Integer>>();
 			
 	SnakesAndLadders(){
-		super(BOARD_SIZE, BOARD_SIZE);
+		//super(BOARD_SIZE, BOARD_SIZE);
 	    dice = new Dice();
-	    // Initialize player position at bottom left corner
-	    int[] player1Pos = new int[]{9, 0};
-	    int[] player2Pos = new int[]{9, 0};
+	    // Initializes player position at bottom left corner
+	    ArrayList<Integer> player1Pos = new ArrayList<Integer>(Arrays.asList(9, 0));
+	    ArrayList<Integer> player2Pos = new ArrayList<Integer>(Arrays.asList(9, 0));
+	    // Updates ArrayList with start positions
 	    positionOfPlayers.put(1,player1Pos);
 	    positionOfPlayers.put(2,player2Pos);
-	    System.out.println("Setting player positions as bottom left");
 	    initializeButtons();
-	    System.out.println("Initializing buttons");
 	
-	    // Snakes
-	    specialSpaces.put(new int[]{0, 8}, new int[]{3, 5});
-	    specialSpaces.put(new int[]{1, 2}, new int[]{4, 2});
-	    specialSpaces.put(new int[]{3, 9}, new int[]{4, 8});
-	    specialSpaces.put(new int[]{5, 5}, new int[]{7, 5});
-	    specialSpaces.put(new int[]{7, 2}, new int[]{8, 3});
+	    // Creates snakes	    
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(0, 8)), new ArrayList<Integer>(Arrays.asList(3, 5)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(1, 2)), new ArrayList<Integer>(Arrays.asList(4, 2)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(3, 9)), new ArrayList<Integer>(Arrays.asList(4, 8)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(5, 5)), new ArrayList<Integer>(Arrays.asList(7, 5)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(7, 2)), new ArrayList<Integer>(Arrays.asList(8, 3)));	    
 	
-	    // Ladders
-	    specialSpaces.put(new int[]{3, 3}, new int[]{1, 3});
-	    specialSpaces.put(new int[]{4, 3}, new int[]{1, 6});
-	    specialSpaces.put(new int[]{7, 0}, new int[]{6, 1});
-	    specialSpaces.put(new int[]{7, 8}, new int[]{6, 8});
-	    specialSpaces.put(new int[]{9, 7}, new int[]{8, 6});
-	    System.out.println("Adding special spaces");
+	    // Creates ladders
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(3, 3)), new ArrayList<Integer>(Arrays.asList(1, 3)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(4, 3)), new ArrayList<Integer>(Arrays.asList(1, 6)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(7, 0)), new ArrayList<Integer>(Arrays.asList(6, 1)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(7, 8)), new ArrayList<Integer>(Arrays.asList(6, 8)));
+	    specialSpaces.put(new ArrayList<Integer>
+	    	(Arrays.asList(9, 7)), new ArrayList<Integer>(Arrays.asList(8, 6)));
 	}
 
-	/*
 	public void initializeButtons() {
+		/*
 	    for (int i = 0; i <= 10; i++) {
 	    	for (int j = 0; j <= 10; j++) {
-	    		
 	    		environment.gameBoard[i] = new JButton();
 	    		environment.gameBoard[i].setText("");
 	    		environment.gameBoard[i].addActionListener(new ticTacToeListener());
 	    	}
-	     
-
+	    	
 	      add(gameBoard[i]); //adds this button to JPanel (note: no need for JPanel.add(...)
 	      //because this whole class is a JPanel already
-	    
 	    }
-	  }
 	  */
+	  }
 
 	public void processLogic() {
-		
 	}
 	
 	
-	int roll(){
+	public int roll(){
 		int roll = dice.roll();
 		System.out.println("Rolling dice --> " + Integer.toString(roll));
-		return roll; // Change this back to dice.roll() after tests
-		
+		return roll;	
 	}
 
-	void advancePlayer(int player, int diceRoll){
-	    int[] currentPosition = positionOfPlayers.get(player);
+	public void advancePlayer(int player, int diceRoll){
+	    ArrayList<Integer> currentPosition = positionOfPlayers.get(player);
+	    System.out.println("Player " + turn + "'s position: " + Integer.toString(currentPosition.get(0)) + ", " + Integer.toString(currentPosition.get(1)));
 	    
 	    // If row is odd
-	    if ((currentPosition[0]) % 2 == 1) { 
-	      if ((9 - diceRoll) >= currentPosition[1]) { // If column + roll <= right edge of board
-	      	currentPosition[1] += diceRoll;
-	      	System.out.println("Advancing player " + turn + " to " + Integer.toString(currentPosition[0]) + ", " + Integer.toString(currentPosition[1]));
+	    if ((currentPosition.get(0)) % 2 == 1) { 
+	      if ((9 - diceRoll) >= currentPosition.get(1)) { // If column + roll <= right edge of board
+	      	currentPosition.set(1, currentPosition.get(1) + diceRoll);
+	      	System.out.println("Advancing to " + Integer.toString(currentPosition.get(0)) + ", " + Integer.toString(currentPosition.get(1)));
 	      }
 	      
-	      else { // Moves carry over to the row above
-	        int carryOverMoves = 9 - currentPosition[1]; 
-	        currentPosition[1] = 9 - carryOverMoves + 1;
-	        currentPosition[0] -= 1;
-	        System.out.println("Advancing player " + turn + " to " + Integer.toString(currentPosition[0]) + ", " + Integer.toString(currentPosition[1]));
+	      else { // Moves carry over moves to the row above
+	        int carryOverMoves = 9 - currentPosition.get(1);
+	        int diceRollMovesLeft = diceRoll - carryOverMoves;
+	        currentPosition.set(1, 9 - diceRollMovesLeft + 1);
+	        currentPosition.set(0, currentPosition.get(0) - 1);
+	        System.out.println("Advancing Player " + turn + " to " + Integer.toString(currentPosition.get(0)) + ", " + Integer.toString(currentPosition.get(1)));
 	      }
-    }
-      
-    // If roll is even
-    else {
-      if ((currentPosition[1] - diceRoll) >= 0) { // If column + roll <= left edge of board
-      	currentPosition[1] -= diceRoll;
-      	System.out.println("Advancing player " + turn + " to " + Integer.toString(currentPosition[0]) + ", " + Integer.toString(currentPosition[1]));
-      }
-      
-      else { // Moves carry over to the row above
-        int carryOverMoves = diceRoll - currentPosition[1]; 
-        currentPosition[1] = carryOverMoves - 1;
-        currentPosition[0] -= 1;
-        System.out.println("Advancing player " + turn + " to " + Integer.toString(currentPosition[0]) + ", " + Integer.toString(currentPosition[1]));
-      }
-      
-    currentPosition = checkSnakeOrLadder(currentPosition);
-    }
-    positionOfPlayers.put(player, currentPosition);
+	    }
+	      
+	    // If row is even
+	    else {
+	      if ((currentPosition.get(1) - diceRoll) >= 0) { // If column + roll <= left edge of board
+	      	currentPosition.set(1,  currentPosition.get(1) - diceRoll);
+	      	System.out.println("Advancing Player " + turn + " to " + Integer.toString(currentPosition.get(0)) + ", " + Integer.toString(currentPosition.get(1)));
+	      }
+	      
+	      else { // Moves carry over moves to the row above
+	        int carryOverMoves = diceRoll - currentPosition.get(1); 
+	        currentPosition.set(1, carryOverMoves - 1);
+	        currentPosition.set(0, currentPosition.get(0) - 1);
+	        System.out.println("Advancing Player " + turn + " to " + Integer.toString(currentPosition.get(0)) + ", " + Integer.toString(currentPosition.get(1)));
+	      }
+	    }
+	    currentPosition = checkSnakeOrLadder(currentPosition); // Moves the player if on a snake or ladder
+	    positionOfPlayers.put(player, currentPosition); // Updates player position
 	}
 
-	int[] checkSnakeOrLadder(int[] position){
-    // Returns end of a snake or ladder
+	
+	public ArrayList<Integer> checkSnakeOrLadder(ArrayList<Integer> position){
+    // Returns end position of a snake or ladder
 	    if (specialSpaces.containsKey(position)) {
-	    	System.out.println("Following snake/ladder to " + Integer.toString(specialSpaces.get(position)[0]) + ", " + Integer.toString(specialSpaces.get(position)[0]));
-	      return specialSpaces.get(position);
+	    	System.out.println("Following snake/ladder to " + Integer.toString(specialSpaces.get(position).get(0)) + ", " + Integer.toString(specialSpaces.get(position).get(1)));
+	    	return specialSpaces.get(position);
 	    }
 	    else {
 	    	System.out.println("No snake/ladder found");
-	      return position;
+	    	return position;
 	    }
 	}
-        
-	void checkWinner() {
-		System.out.println("Checking for a winner");
+	
+	public boolean foundWinner() {
 		for (int i = 1; i <= 2; i++) {
-			if (positionOfPlayers.get(i).equals(new int[]{0, 0})) {
-		    	System.out.println("Player " + Integer.toString(i) + " won!");
+			if ((positionOfPlayers.get(i).get(0) <= 0) && (positionOfPlayers.get(i).get(1) <= 0)) {
+				System.out.println("Player " + Integer.toString(i) + " won!");
+		    	return true;
 		    }
 		}
-	  }
+		return false;
+	}
 
-	void playerTurn(){
-		while (true) {
-			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter something here: ");
+	public void playGame(){
+		while (!foundWinner()) {
+			Scanner sc = new Scanner(System.in); // Replace scanner with click listening (when we connect to UI)
+			System.out.print("Enter any number & press enter to continue: ");
 			int i = sc.nextInt();
 			
-			
-			// 1) need to listen for click on dice button
-			advancePlayer(turn, roll());     // 2) advance player based on dice number
+			advancePlayer(turn, 7); // 2) advance player based on dice number // HARD CODED 7 FOR ROLL
 		      
-			if (turn == 1) {
+			/*
+			 if (turn == 1) {
 				System.out.println("Player 2's turn is up!");
 				turn = 2;
 			}
@@ -148,20 +149,12 @@ class SnakesAndLadders extends GridGame{
 				turn = 1;
 			    }
 			}
-			// If winner, break
+			*/
 		}
+	}
 	
-//	public void printBoard() {
-//		for (int i = 0; i < BOARD_SIZE; i++) {
-//			for (int j = 0; j < BOARD_SIZE; j++) {
-//				System.out.println();
-//			}
-//		}
-//	}
-	
-	public static void main() {
+	public static void main(String args[]) {
 		 SnakesAndLadders SNL = new SnakesAndLadders();
-		 SNL.playerTurn();
-		
+		 SNL.playGame();
 	}
 }
