d70cf95bb4a66dbe35d2ec7ea2ba79ce3c58a686
Josh Jacoby <joshuadjacoby@gmail.com>
2018-03-17 16:31:21 -0700

    Added player profiles and state, just needs game over function in gui

--- INF-122-Final/src/GUI.java
+++ INF-122-Final/src/GUI.java
@@ -10,14 +10,25 @@ import java.net.URL;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame implements ActionListener {
+	private static GUI gui;
+	private static final int introWidth = 700;
+	private static final int introHeight = 270;
 	String gameChoice;
+<<<<<<< Updated upstream
 	GridGame game;
 	JPanel introPanel;
+=======
+	JPanel introPanel;
+	JTextField playerOneName;
+	JTextField playerTwoName;
+	PlayerManager pm = PlayerManager.getInstance();
+	State s = State.getInstance();
+>>>>>>> Stashed changes
 
 	public GUI(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(700, 270);
+		setSize(introWidth, introHeight);
 		setLocationRelativeTo(null);
 		gameChoice = "Tic-Tac-Toe";
 		try {
@@ -28,6 +39,13 @@ public class GUI extends JFrame implements ActionListener {
 		introPanel.setLayout(new BoxLayout(introPanel, BoxLayout.PAGE_AXIS));
 		chooseGame();
 		setVisible(true);
+	}
+
+	public static GUI getInstance() {
+		if (gui == null) {
+			gui = new GUI("G6 Games");
+		}
+		return gui;
 	}
 
 	private void chooseGame() {
@@ -46,19 +64,33 @@ public class GUI extends JFrame implements ActionListener {
 		JPanel playerOnePanel = new JPanel();
 		playerOnePanel.setOpaque(false);
 		playerOnePanel.setLayout(new BoxLayout(playerOnePanel, BoxLayout.PAGE_AXIS));
+<<<<<<< Updated upstream
 		JLabel playerOne = new JLabel("Player One");
 		playerOne.setForeground(Color.WHITE);
 		JTextField playerOneName = new JTextField();
 		playerOnePanel.add(playerOne);
+=======
+		JLabel playerOneLabel = new JLabel("Player 1");
+		playerOneLabel.setForeground(Color.WHITE);
+		playerOneName = new JTextField("Player 1");
+		playerOnePanel.add(playerOneLabel);
+>>>>>>> Stashed changes
 		playerOnePanel.add(playerOneName);
 
 		JPanel playerTwoPanel = new JPanel();
 		playerTwoPanel.setOpaque(false);
 		playerTwoPanel.setLayout(new BoxLayout(playerTwoPanel, BoxLayout.PAGE_AXIS));
+<<<<<<< Updated upstream
 		JLabel playerTwo = new JLabel("Player Two");
 		playerTwo.setForeground(Color.WHITE);
 		JTextField playerTwoName = new JTextField();
 		playerTwoPanel.add(playerTwo);
+=======
+		JLabel playerTwoLabel = new JLabel("Player 2");
+		playerTwoLabel.setForeground(Color.WHITE);
+		playerTwoName = new JTextField("Player 2");
+		playerTwoPanel.add(playerTwoLabel);
+>>>>>>> Stashed changes
 		playerTwoPanel.add(playerTwoName);
 
 		players.add(playerOnePanel);
@@ -83,6 +115,7 @@ public class GUI extends JFrame implements ActionListener {
 
 	private void startGame() {
 		remove(introPanel);
+<<<<<<< Updated upstream
 		setBounds(300, 200, 300, 300);
 		add(game);
 		validate();
@@ -91,6 +124,15 @@ public class GUI extends JFrame implements ActionListener {
 	public void gameOver() {
 		remove(game);
 		setBounds(300, 200, 300, 100);
+=======
+		add(s.getGame());
+		validate();
+	}
+
+	public void gameOver(String winningPlayer, String losingPlayer) {
+		remove(s.getGame());
+		setSize(introWidth, introHeight);
+>>>>>>> Stashed changes
 		add(introPanel);
 		validate();
 	}
@@ -101,7 +143,23 @@ public class GUI extends JFrame implements ActionListener {
 			JComboBox cb = (JComboBox)e.getSource();
 			gameChoice = cb.getSelectedItem().toString();
 		} else if (e.getSource() instanceof JButton) {
-			game = GameFactory.getInstance().makeGame(gameChoice, this);
+			GameBoard game = GameFactory.getInstance().makeGame(gameChoice, this);
+			Player playerOne;
+			Player playerTwo;
+			if (!pm.playerExists(playerOneName.getText())) {
+				playerOne = new Player(playerOneName.getText(), pm.getPlayerIDCount());
+				pm.incrementPlayerIDCount();
+			} else {
+				playerOne = pm.findPlayer(playerOneName.getText());
+			}
+			if (!pm.playerExists(playerTwoName.getText())) {
+				playerTwo = new Player(playerTwoName.getText(), pm.getPlayerIDCount());
+				pm.incrementPlayerIDCount();
+			} else {
+				playerTwo = pm.findPlayer(playerTwoName.getText());
+			}
+			s.updatePlayers(playerOne, playerTwo);
+			s.updateGame(game);
 			startGame();
 		}
 	}
--- INF-122-Final/src/Player.java
+++ INF-122-Final/src/Player.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 class Player{
 	private int playerNum;
@@ -24,3 +25,48 @@ class Player{
 	
 	
 }
+=======
+import java.awt.*;
+
+class Player{
+	private String name;
+    private int playerID;
+    private int numberOfWins;
+    private int numberOfLosses;
+	
+	Player(String name, int id){
+	    this.name = name;
+	    this.playerID = id;
+	    this.numberOfWins = 0;
+	    this.numberOfLosses = 0;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPlayerID() {
+		return playerID;
+	}
+
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID;
+	}
+
+	public int getNumberOfWins() {
+		return numberOfWins;
+	}
+
+	public void incrementWins() {
+		this.numberOfWins++;
+	}
+
+	public int getNumberOfLosses() {
+		return numberOfLosses;
+	}
+
+	public void incrementLosses() {
+		this.numberOfLosses++;
+	}
+}
+>>>>>>> Stashed changes
