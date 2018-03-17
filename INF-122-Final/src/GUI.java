347c030aee5074a6bafd11e6b6a49bb04debce70
Josh Jacoby <joshuadjacoby@gmail.com>
2018-03-17 13:50:42 -0700

    Added strings for player names

--- INF-122-Final/src/GUI.java
+++ INF-122-Final/src/GUI.java
@@ -13,6 +13,8 @@ public class GUI extends JFrame implements ActionListener {
 	String gameChoice;
 	GameBoard game;
 	JPanel introPanel;
+	String playerOne;
+	String playerTwo;
 
 	public GUI(String title) {
 		super(title);
@@ -20,6 +22,8 @@ public class GUI extends JFrame implements ActionListener {
 		setSize(700, 270);
 		setLocationRelativeTo(null);
 		gameChoice = "Tic-Tac-Toe";
+		playerOne = "Player 1";
+		playerTwo = "Player 2";
 		try {
 			introPanel = new JPanelWithBackground("src/images/space.jpg");
 		} catch(IOException e){
@@ -46,19 +50,19 @@ public class GUI extends JFrame implements ActionListener {
 		JPanel playerOnePanel = new JPanel();
 		playerOnePanel.setOpaque(false);
 		playerOnePanel.setLayout(new BoxLayout(playerOnePanel, BoxLayout.PAGE_AXIS));
-		JLabel playerOne = new JLabel("Player One");
-		playerOne.setForeground(Color.WHITE);
-		JTextField playerOneName = new JTextField();
-		playerOnePanel.add(playerOne);
+		JLabel playerOneLabel = new JLabel("Player 1");
+		playerOneLabel.setForeground(Color.WHITE);
+		JTextField playerOneName = new JTextField("Player 1");
+		playerOnePanel.add(playerOneLabel);
 		playerOnePanel.add(playerOneName);
 
 		JPanel playerTwoPanel = new JPanel();
 		playerTwoPanel.setOpaque(false);
 		playerTwoPanel.setLayout(new BoxLayout(playerTwoPanel, BoxLayout.PAGE_AXIS));
-		JLabel playerTwo = new JLabel("Player Two");
-		playerTwo.setForeground(Color.WHITE);
-		JTextField playerTwoName = new JTextField();
-		playerTwoPanel.add(playerTwo);
+		JLabel playerTwoLabel = new JLabel("Player 2");
+		playerTwoLabel.setForeground(Color.WHITE);
+		JTextField playerTwoName = new JTextField("Player 2");
+		playerTwoPanel.add(playerTwoLabel);
 		playerTwoPanel.add(playerTwoName);
 
 		players.add(playerOnePanel);
@@ -87,9 +91,9 @@ public class GUI extends JFrame implements ActionListener {
 		validate();
 	}
 
-	public void gameOver() {
+	public void gameOver(/*String winningPlayer, String losingPlayer*/) {
 		remove(game);
-		setBounds(300, 200, 300, 100);
+		setSize(700, 270);
 		add(introPanel);
 		validate();
 	}
