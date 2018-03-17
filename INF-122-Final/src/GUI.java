d5e2e7391d5d824fb4d8b4cf796ac3213bf5d0c1
Josh Jacoby <joshuadjacoby@JOSH-SURFACE.localdomain>
2018-03-17 12:49:23 -0700

    Updated starting menu with player name inputs and better look

--- INF-122-Final/src/GUI.java
+++ INF-122-Final/src/GUI.java
@@ -1,6 +1,12 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame implements ActionListener {
@@ -11,9 +17,15 @@ public class GUI extends JFrame implements ActionListener {
 	public GUI(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(300, 200, 300, 100);
+		setSize(700, 270);
+		setLocationRelativeTo(null);
 		gameChoice = "Tic-Tac-Toe";
-		introPanel = new JPanel();
+		try {
+			introPanel = new JPanelWithBackground("src/images/space.jpg");
+		} catch(IOException e){
+
+		}
+		introPanel.setLayout(new BoxLayout(introPanel, BoxLayout.PAGE_AXIS));
 		chooseGame();
 		setVisible(true);
 	}
@@ -22,11 +34,49 @@ public class GUI extends JFrame implements ActionListener {
 		String gameNames[] = {"Tic-Tac-Toe", "Checkers", "Othello", "Snakes and Ladders"};
 		JComboBox gameList = new JComboBox(gameNames);
 		gameList.addActionListener(this);
-		JTextArea gameText = new JTextArea("What game would you like to play?");
+		JLabel titleText = new JLabel("G6 Games");
+		titleText.setForeground(Color.WHITE);
+		titleText.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		titleText.setFont(titleText.getFont().deriveFont(100f));
+
+		JPanel players = new JPanel();
+		players.setOpaque(false);
+		players.setLayout(new BoxLayout(players, BoxLayout.LINE_AXIS));
+
+		JPanel playerOnePanel = new JPanel();
+		playerOnePanel.setOpaque(false);
+		playerOnePanel.setLayout(new BoxLayout(playerOnePanel, BoxLayout.PAGE_AXIS));
+		JLabel playerOne = new JLabel("Player One");
+		playerOne.setForeground(Color.WHITE);
+		JTextField playerOneName = new JTextField();
+		playerOnePanel.add(playerOne);
+		playerOnePanel.add(playerOneName);
+
+		JPanel playerTwoPanel = new JPanel();
+		playerTwoPanel.setOpaque(false);
+		playerTwoPanel.setLayout(new BoxLayout(playerTwoPanel, BoxLayout.PAGE_AXIS));
+		JLabel playerTwo = new JLabel("Player Two");
+		playerTwo.setForeground(Color.WHITE);
+		JTextField playerTwoName = new JTextField();
+		playerTwoPanel.add(playerTwo);
+		playerTwoPanel.add(playerTwoName);
+
+		players.add(playerOnePanel);
+		players.add(playerTwoPanel);
+
+		JPanel chooseGamePanel = new JPanel();
+		chooseGamePanel.setOpaque(false);
+		JLabel gameText = new JLabel("What game would you like to play?");
+		gameText.setForeground(Color.WHITE);
+		gameText.setFont(gameText.getFont().deriveFont(20f));
 		JButton gameSelectButton = new JButton("Let's go!");
+		gameSelectButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		chooseGamePanel.add(gameText);
+		chooseGamePanel.add(gameList);
 		gameSelectButton.addActionListener(this);
-		introPanel.add(gameText);
-		introPanel.add(gameList);
+		introPanel.add(titleText);
+		introPanel.add(players);
+		introPanel.add(chooseGamePanel);
 		introPanel.add(gameSelectButton);
 		add(introPanel);
 	}
@@ -53,6 +103,25 @@ public class GUI extends JFrame implements ActionListener {
 		} else if (e.getSource() instanceof JButton) {
 			game = GameFactory.getInstance().makeGame(gameChoice, this);
 			startGame();
+		}
+	}
+
+	private class JPanelWithBackground extends JPanel {
+
+		private Image backgroundImage;
+
+		// Some code to initialize the background image.
+		// Here, we use the constructor to load the image. This
+		// can vary depending on the use case of the panel.
+		public JPanelWithBackground(String fileName) throws IOException {
+			backgroundImage = ImageIO.read(new File(fileName));
+		}
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+
+			// Draw the background image.
+			g.drawImage(backgroundImage, 0, 0, this);
 		}
 	}
 }
