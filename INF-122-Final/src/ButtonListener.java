e96910ace17672c480152a801c1810492247cb59
fguan0529 <guan.fanny@gmail.com>
2018-03-19 00:03:30 -0700

    Removed ButtonListener class we don't need anymore

--- INF-122-Final/src/ButtonListener.java
+++
@@ -1,6 +0,0 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public abstract class ButtonListener implements ActionListener {
-    public abstract void actionPerformed(ActionEvent e);
-}
